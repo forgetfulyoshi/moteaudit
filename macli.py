24f65bc0de1d985f9107e73c3bfdb28b65ec1cc1
Ben Anglin <ben.anglin@gmail.com>
2012-05-10 18:03:19 -0400

    Added proper argument handling to the interface and added a 'clear' command

--- macli.py
+++ macli.py
@@ -1,4 +1,8 @@
+#! /usr/bin/python
+
+import argparse
 import cmd
+import os
 import signal
 import sys
 
@@ -14,29 +18,51 @@ class MoteAuditPrompt(cmd.Cmd):
 
     def do_ls(self, args):
         """get details of a mote"""
-        args = args.split()
+        parser = argparse.ArgumentParser(description='List all known motes')
+        parser.add_argument('mote_id', type=int, nargs='?',
+                            help='Give information for a specifc mote')
+
+        try:
+            args = parser.parse_args(args.split())
+        except SystemExit as e:
+            return
+
+        mote = args.mote_id
+
+        if mote:
+            motes = self.mote_registry.summary()
+            if mote in motes:
+                print str(hex(mote)) + '\t' \
+                      + str(motes[mote]['sent']) + '\t' \
+                      + str(motes[mote]['recv'])
         
-        if len(args) is 0:
+        else:
             motes = self.mote_registry.summary()
             for mote in motes:
                 print str(hex(mote)) + '\t' \
                       + str(motes[mote]['sent']) + '\t' \
                       + str(motes[mote]['recv'])
 
-        if len(args) is 1:
-            motes = self.mote_registry.summary()
-            mote = int(args[0])
-            if mote in motes:
-                print str(hex(mote)) + '\t' \
-                      + str(motes[mote]['sent']) + '\t' \
-                      + str(motes[mote]['recv'])
 
     def do_packets(self, args):
-        if len(args) > 1:
-            pkts = self.mote_registry.packets[0:int(args)]
-            
-            for pkt in pkts:
-                print pkt
+        parser = argparse.ArgumentParser(description='Get a range of packets')
+        parser.add_argument('count', type=int, nargs=1,
+                            help='Return this many of the most recent packets')
+
+        try:
+            args = parser.parse_args(args.split())
+        except SystemExit as e:
+            return
+
+        count = args.count.pop()
+
+        pkts = self.mote_registry.packets[:-count]
+        
+        for pkt in pkts:
+            print pkt
+
+    def do_clear(self, args):
+        os.system('clear')
 
     def do_exit(self, args):
         self.quit()
@@ -50,12 +76,12 @@ class MoteAuditPrompt(cmd.Cmd):
     def catch(self, signal, line):
         self.quit()
 
-    def quit(self):
+    def quit(self, signal=None, frame=None):
         self.mote_registry.is_running = False
         self.mote_registry.join()
         sys.exit()
 
 if __name__ == '__main__':
     prompt = MoteAuditPrompt()
-    signal.signal(signal.SIGINT, prompt.sig_test)
+    signal.signal(signal.SIGINT, prompt.quit)
     prompt.cmdloop()
