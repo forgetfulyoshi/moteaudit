6eb883d835551c3f38b1e764ea725a6693072406
Ben Anglin <ben.anglin@gmail.com>
2012-05-17 16:26:49 -0400

    Made the interface more user friendly

--- motesend.py
+++ motesend.py
@@ -10,21 +10,26 @@ def inject(source, dest, group, payload):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Arbitrary packet injection for TinyOS motes")
-    parser.add_argument('source', type=int, nargs=1, 
-                        help="Source mote to spoof")
-    parser.add_argument('destination', type=int, nargs='?', default=0xFFFF,
-                        help="Destination for packet - defaults to flooding")
-    parser.add_argument('group', type=int, nargs=1,
-                        help="Mote group")
+    parser.add_argument('-s', '--source', type=int, nargs=1, default=[0L],
+                        help="Source mote to spoof - default is zero")
+    parser.add_argument('-d', '--destination', type=int, nargs=1, default=[0xFFFF],
+                        help="Destination for packet - default is flooding")
+    parser.add_argument('-g', '--group', type=int, nargs=1, default=[0L],
+                        help="Mote group - default is zero")
     parser.add_argument('payload', type=str, nargs=1, default='',
-                        help="Packet payload")
+                        help="Packet payload. Should be a comma-separated list \
+                        of decimals (e.g. 0,0,1,55)")
     
     args = parser.parse_args()
-    print args
     
     source = args.source.pop()
-    dest = args.destination
+    dest = args.destination.pop()
     group = args.group.pop()
-    payload = [int(x) for x in args.payload.pop().split(',')]
+    
+    try:
+        payload = [int(x) for x in args.payload.pop().split(',')]
+    except ValueError:
+        print "[-] Invalid payload format"
+        sys.exit()
 
     inject(source, dest, group, payload)
